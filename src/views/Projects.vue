1be839e45383ab49ae735b1cbea83003f991d4a3
Sergiyss <weert19933@gmail.com>
2025-05-01 22:11:02 +0300

    Перевод для веб проектов

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -26,11 +26,11 @@
               <div class="project-content p-4">
                 <h3 class="project-title text-xl font-semibold mb-2">{{ project.title }}</h3>
 
-
                 <hr class="my-2 border-t border-gray-300"/>
 
                 <p class="project-description text-gray-700 mb-4">
-                  {{ project.description }} </p>
+                  {{ project.description }}
+                </p>
                 <div class="project-tags mb-2">
                   <span
                       class="project-tag inline-block bg-gray-200 text-gray-700 text-sm px-2 py-1 rounded mr-2 mb-2"
@@ -41,12 +41,10 @@
                   </span>
                 </div>
                 <div class="project-links flex justify-between text-blue-600 text-sm">
-                  <!-- Отображение ссылки на GitHub если она есть -->
                   <span v-if="project.github">
                     <a :href="project.github" target="_blank" class="hover:underline">GitHub 🔗</a>
                   </span>
 
-                  <!-- Отображение ссылки на Deploy если она есть -->
                   <span v-if="project.link">
                     <a :href="project.link" target="_blank" class="rounded text-white bg-amber-500 p-1 hover:underline">Play Market</a>
                   </span>
@@ -67,84 +65,74 @@
             v-for="(project, index) in web_projects"
             :key="index"
         >
-<!--          <router-link-->
-<!--              :to="{ path: '/blog', query: { id: project.id } }"-->
-<!--              class="block"-->
-<!--          >-->
-            <div class="card-inner">
-              <img
-                  class="project-img w-full h-48 object-cover"
-                  :src="getImageUrl(project.image)"
-                  :alt="project.title"
+          <div class="card-inner">
+            <img
+                class="project-img w-full h-48 object-cover"
+                :src="getImageUrl(project.image)"
+                :alt="project.title[locale]"
+            />
+
+            <div class="project-content p-4">
+              <h3 class="project-title text-xl font-semibold mb-2">{{ project.title[locale] }}</h3>
+
+              <p class="project-description text-gray-700 mb-4">
+                {{ project.description[locale] }}
+              </p>
+
+              <hr class="my-2 border-t border-gray-300"/>
+
+              <p
+                  class="project-description text-gray-700 mb-4"
+                  v-html="project.fullDescription[locale].replace(/\n/g, '<br>')"
               />
 
-              <div class="project-content p-4">
-                <h3 class="project-title text-xl font-semibold mb-2">{{ project.title }}</h3>
+              <div class="project-tags mb-2">
+                <span
+                    class="project-tag inline-block bg-gray-200 text-gray-700 text-sm px-2 py-1 rounded mr-2 mb-2"
+                    v-for="(tag, tagIndex) in project.tags"
+                    :key="tagIndex"
+                >
+                  {{ tag }}
+                </span>
+              </div>
 
-                <p class="project-description text-gray-700 mb-4">
-                  {{ project.description }}
-                </p>
+              <div class="project-links flex justify-between text-blue-600 text-sm">
+                <span v-if="project.github">
+                  <a :href="project.github" target="_blank" class="hover:underline">GitHub 🔗</a>
+                </span>
 
-                <hr class="my-2 border-t border-gray-300"/>
-
-                <p
-                    class="project-description text-gray-700 mb-4"
-                    v-html="project.fullDescription.replace(/\n/g, '<br>')"
-                />
-
-                <div class="project-tags mb-2">
-                  <span
-                      class="project-tag inline-block bg-gray-200 text-gray-700 text-sm px-2 py-1 rounded mr-2 mb-2"
-                      v-for="(tag, tagIndex) in project.tags"
-                      :key="tagIndex"
-                  >
-                    {{ tag }}
-                  </span>
-                </div>
-
-
-                <div class="project-links flex justify-between text-blue-600 text-sm">
-                  <!-- Отображение ссылки на GitHub если она есть -->
-                  <span v-if="project.github">
-                    <a :href="project.github" target="_blank" class="hover:underline">GitHub 🔗</a>
-                  </span>
-
-                  <!-- Отображение ссылки на Deploy если она есть -->
-                  <span v-if="project.link">
-                    <a :href="project.link" target="_blank" class="rounded text-white bg-amber-500 p-1 hover:underline">Play Market</a>
-                  </span>
-                </div>
+                <span v-if="project.link">
+                  <a :href="project.link" target="_blank" class="rounded text-white bg-amber-500 p-1 hover:underline">Play Market</a>
+                </span>
               </div>
             </div>
-<!--          </router-link>-->
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-
-
 <script setup>
-  import {ref, computed} from 'vue'
-  import { projects } from '@/data/projects';
-  import { web_projects } from '@/data/web_projects';
-  import {useI18n} from 'vue-i18n';
-  import { getImageUrl } from '@/utils/helpers';
+import {ref, computed} from 'vue'
+import { projects } from '@/data/projects';
+import { web_projects } from '@/data/web_projects';
+import {useI18n} from 'vue-i18n';
+import { getImageUrl } from '@/utils/helpers';
 
-  const {t} = useI18n(); // подключаем $t для переводов
+const {t, locale} = useI18n();
 
-  const itemsPerPage = ref(getItemsPerPage())
-  const currentPage = ref(0)
+const itemsPerPage = ref(getItemsPerPage())
+const currentPage = ref(0)
 
-  function getItemsPerPage() {
-    return window.innerWidth < 768 ? 1 : 3 // 1 на мобилке, 6 на десктопе
-  }
+function getItemsPerPage() {
+  return window.innerWidth < 768 ? 1 : 3
+}
 
-  window.addEventListener('resize', () => {
-    itemsPerPage.value = getItemsPerPage()
-    currentPage.value = 0
-  })
+window.addEventListener('resize', () => {
+  itemsPerPage.value = getItemsPerPage()
+  currentPage.value = 0
+})
 </script>
 
 <style scoped>
@@ -178,12 +166,10 @@
   margin-bottom: 0.5rem;
 }
 
-
 @media (max-width: 767px) {
   .card-inner {
     width: 100%;
     box-sizing: border-box;
   }
 }
-
 </style>
